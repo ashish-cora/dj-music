8aa5b37e911eea456faa3eaeccd13fe7f2be315a
ashish-cora <invoker.god18@gmail.com>
2023-07-19 12:19:16 +0545

    first commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>this is an app to find latest dj music and events</p>
+      <p>version 1.0.0</p>
+    </div>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,5 @@
+export default function EventsPage() {
+  return (
+    <div><h1>My events</h1></div>
+  )
+} 
